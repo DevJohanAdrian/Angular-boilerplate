f761c12e0cc880c28bc8650bc5412fd5996b0140
DevJohanAdrian <jamaringarciabusiness@gmail.com>
2025-08-12 23:53:12 -0500

    feat: not found page

--- src/app/presentation/views/pages/not-found/not-found.component.ts
+++ src/app/presentation/views/pages/not-found/not-found.component.ts
@@ -1,9 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
-  template: `<p>404 works!</p>`,
+  imports: [   CommonModule,
+    RouterLink,
+    MatButtonModule,
+    MatIconModule],
+  templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
